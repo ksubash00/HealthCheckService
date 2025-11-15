65b1f93777c016811634e7c82f69cec4265c165d
ksubash00 <ksubash00@gmail.com>
2025-11-15 17:15:25 -0600

    Add HealthCheckService.Designer.cs

---
+++ HealthCheckService.Designer.cs
@@ -0,0 +1,22 @@
+namespace HealthCheckService
+{
+    partial class HealthCheckService
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            this.ServiceName = "ApplicationHealthCheckService";
+        }
+    }
+}
